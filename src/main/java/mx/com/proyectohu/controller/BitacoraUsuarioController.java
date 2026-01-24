42b281d9093b781b6dbd5f3c2e59315b6a38a786
Miguel Ambrosio <Miguel Ambrosio@badak>
2026-01-24 06:10:59 -0600

    mejoras al codigo, nueva llave para linea columna

--- src/main/java/mx/com/proyectohu/controller/BitacoraUsuarioController.java
+++ src/main/java/mx/com/proyectohu/controller/BitacoraUsuarioController.java
@@ -6,23 +6,12 @@ import org.springframework.web.bind.annotation.RestController;
 import mx.com.proyectohu.dto.BitacoraUsuarioRequestDTO;
 import mx.com.proyectohu.service.BitacoraUsuarioService;
 
-import java.util.ArrayList;
-import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-
-import org.springframework.web.bind.annotation.PutMapping;
-
-
-
 
 @RestController
 @CrossOrigin(origins = "*")
