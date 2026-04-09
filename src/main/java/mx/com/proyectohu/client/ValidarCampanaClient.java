1d1078cbe35d5282beda29795ca6ca237ed24c8f
Miguel Ambrosio <Miguel Ambrosio@badak>
2026-04-09 17:51:39 -0600

    clientes de carga y validacion

---
+++ src/main/java/mx/com/proyectohu/client/ValidarCampanaClient.java
@@ -0,0 +1,74 @@
+package mx.com.proyectohu.client;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+import org.json.JSONObject;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ValidarCampanaClient {
+
+
+
+	private final HttpClient httpClient;
+
+
+	public ValidarCampanaClient() {
+		this.httpClient = HttpClient.newHttpClient();
+	}
+
+
+	
+	public String llamarValidarCampana(String lineaNegocio, Long idTareaCampana){
+		Integer statusCode=null;
+		String body=null;
+		String url= "http://localhost:8080/profuturo/api/v1/valida/extencion/perfil";
+		JSONObject json = new JSONObject();
+		
+		json.put("lineaNegocio", lineaNegocio);
+		json.put("idTareaCampana", idTareaCampana);
+		
+		try {
+			HttpRequest request = HttpRequest.newBuilder()
+					.uri(URI.create(url))
+					.header("Content-Type", "application/json")
+					.POST(HttpRequest.BodyPublishers.ofString(json.toString()))
+					.build();
+
+			HttpResponse<String> response;
+
+			response = httpClient.send(request,	HttpResponse.BodyHandlers.ofString());
+
+
+			
+			statusCode = response.statusCode();
+			body = response.body();
+
+			
+
+
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		if (statusCode == 200) {
+			return body;
+		} else {
+			throw new RuntimeException("Error en la llamada: " + statusCode);
+		}
+		
+
+	}
+
+
+
+
+}
